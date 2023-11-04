10e4b7e386127fa7605feb95bad2f544ba62e250
Ubabar786 <babaruzair@gmail.com>
2023-11-04 16:04:27 +0000

    I have added all features required for the car

---
+++ src/test/java/CarFeature1.java
@@ -0,0 +1,7 @@
+public class CarFeature1 {
+    String tyre;
+    String tyreColour;
+    String tyreWidth;
+    String tyreModel;
+
+}
--- src/test/java/main.java
+++ src/test/java/main.java
@@ -5,5 +5,10 @@ public class main {
         audi.hp="5000";
         audi.doors="2";
         audi.model="s7";
+    CarFeature1 tyre=new CarFeature1();
+    tyre.tyre="4";
+    tyre.tyreColour="blue";
+    tyre.tyreWidth="18";
+    tyre.tyreModel="Yokohama";
     }
 }
